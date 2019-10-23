8165d64f736922b4981c0da2eecd4f439660b424
Cem Keylan <cem@ckyln.com>
2019-10-23 09:22:48 +0300

    change firefox class name

--- config.h
+++ config.h
@@ -36,7 +36,7 @@ static const Rule rules[] = {
 	 */
 	/* class         instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",        NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",     NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",     NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "qutebrowser", NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Surf",	 NULL,	     NULL,       1 << 8,       0,           -1 },
 	{ "tabbed",	 NULL,	     NULL,       1 << 8,       0,           -1 },
